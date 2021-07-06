e797536876dc327fa16b09fab369e16ccae46d88
syuni <motikix@gmail.com>
2021-07-06 20:27:06 +0900

    vue + webpack

---
+++ vue-counter/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Counter: {{ counter }},
+  </div>
+  <div>
+    <span>Clickable Counter: {{ ccounter }}</span>
+    <button @click="increment">Click Me!!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+      ccounter: 0,
+    }
+  },
+  mounted() {
+    fetch('google.com')
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  },
+  methods: {
+    increment() {
+      this.ccounter++
+    },
+  },
+}
+</script>
+
+<style scoped>
+span {
+  display: inline-block;
+  min-width: 180px;
+}
+</style>
---
+++ vue-counter/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: {
+    index: path.join(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/[name]-[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      inject: 'head',
+    }),
+    new VueLoaderPlugin(),
+  ]
+}
