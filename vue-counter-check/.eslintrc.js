e8e6f2a1db246faa6c1267c8ac67468b53f013c1
syuni <motikix@gmail.com>
2021-07-13 22:28:09 +0900

    fix

--- vue-counter-check/.eslintrc.js
+++ vue-counter-check/.eslintrc.js
@@ -5,9 +5,4 @@ module.exports = {
     node: true,
   },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'double'],
-    'comma-dangle': ['error', 'never'],
-  },
 }
--- vue-counter-check/src/App.vue
+++ vue-counter-check/src/App.vue
@@ -23,6 +23,7 @@ export default {
   },
   methods: {
     increment() {
+      const unused = 'ESLint test'
       this.ccounter++
     },
   },
